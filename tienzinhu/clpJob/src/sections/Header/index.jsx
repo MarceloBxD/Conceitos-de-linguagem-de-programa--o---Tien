60ca1002e73b24f587789ce202b60b68b3b67acb
Marcelo Bracet <marcelo.bracet1@gmail.com>
2022-12-07 17:26:23 -0300

    first commit | initializing

---
+++ tienzinhu/clpJob/src/sections/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Flex, Text, Avatar } from "@chakra-ui/react";
+
+export const Header = () => {
+  return (
+    <Flex w="100%" justify="space-between">
+      <Text />
+      <Avatar />
+    </Flex>
+  );
+};
